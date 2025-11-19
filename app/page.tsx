6445f5cd62d05e592b4de23f708c76003b5ee68e
Igor <47291559+r0gi@users.noreply.github.com>
2025-11-19 21:36:18 +1300

    Implement search filter

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,7 @@ import { getBestDeal } from "@/utils";
 
 export default function Home() {
   const [restaurants, setRestaurants] = useState<Restaurant[]>([]);
+  const [search, setSearch] = useState("");
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(true);
 
@@ -35,8 +36,14 @@ export default function Home() {
     })();
   }, []);
 
+  // filter by name
+  const filteredRestaurants = restaurants.filter(r => 
+    r.name.toLowerCase().includes(search.toLowerCase().trim()) ||
+    r.cuisines.some(cuisine => cuisine.toLowerCase().includes(search.toLowerCase().trim()))
+  );
+
   // sort restaurants by best deals first
-  restaurants.sort((a, b) =>
+  filteredRestaurants.sort((a, b) =>
     Number(getBestDeal(b.deals)?.discount ?? 0) - Number(getBestDeal(a.deals)?.discount ?? 0)
   );
 
@@ -45,7 +52,13 @@ export default function Home() {
       <main className="flex min-h-screen w-full max-w-3xl flex-col items-center pb-8 px-2 bg-white dark:bg-black sm:items-center">
         <div className="flex items-center w-full gap-2 border-b border-gray-200 mb-4">
           <SearchIcon size={16} />
-          <input type="text" className="w-full py-2" placeholder="e.g. chinese, pizza" />
+          <input
+            type="text"
+            className="w-full py-2"
+            placeholder="e.g. chinese, pizza"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
         </div>
 
         {!loading && error && (
@@ -54,7 +67,7 @@ export default function Home() {
 
         {!loading && !error && (
           <div className="grid w-full gap-12 text-center sm:text-left sm:grid-cols-2">
-            {restaurants.map(restaurant =>
+            {filteredRestaurants.map(restaurant =>
               <RestaurantCard key={restaurant.objectId} restaurant={restaurant} />
             )}
           </div>
