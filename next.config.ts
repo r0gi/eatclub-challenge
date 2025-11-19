3c3cfa85bb5b2514e14c10db18c598744e547456
Igor <47291559+r0gi@users.noreply.github.com>
2025-11-19 20:36:24 +1300

    Allow images from URL domains in API response

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      new URL('https://dinnerdeal.backendless.com/**'),
+      new URL('https://demo.eccdn.com.au/**')
+    ],
+  }
 };
 
 export default nextConfig;
