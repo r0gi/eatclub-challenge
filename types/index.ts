2072d8d187694d30e33e06c5c23296645b5a7dfa
Igor <47291559+r0gi@users.noreply.github.com>
2025-11-19 18:44:06 +1300

    Create types for API response

---
+++ types/index.ts
@@ -0,0 +1,25 @@
+export type Deal = {
+  objectId: string;
+  discount: string;     // NOTE: seems to be percentage but represented as string
+  dineIn: string;       // "true" | "false"
+  lightning: string;    // "true" | "false"
+  open?: string;
+  close?: string;
+  qtyLeft: string;
+}
+
+export type Restaurant = {
+  objectId: string;
+  name: string;
+  address1: string;
+  suburb: string;
+  cuisines: string[];
+  imageLink: string;
+  open: string;
+  close: string;
+  deals: Deal[];
+}
+
+export interface RestaurantResponse {
+  restaurants: Restaurant[];
+}
